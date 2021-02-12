1826db079b6007067316b95e3ef8adc59dfd0d0e
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-02-12 18:53:34 +0500

    Add random

--- js/script.js
+++ js/script.js
@@ -1,21 +1,19 @@
 import { data } from './data.js'
 
-// let place = newImg.getBoundingClientRect()
-
 window.onload = (evt) => {
-	let index = 1
 
 	for(let d of data) {
+
 		let x = randomRange(0, window.innerWidth)
 		let y = randomRange(0, window.innerHeight)
 
 		let img = document.createElement('IMG')
 		img.src = `./img/${d}.png`
-		img.dataset.id = index
-		// img
+		img.dataset.id = d
+		img.style.cssText = `left: ${Math.abs(x - 128)}px; top:${Math.abs(y - 128)}px;`
+		img.onclick = getId
 
 		box.appendChild(img)
-		index++
 	}
 }
 
@@ -23,3 +21,21 @@ function randomRange(minNum, maxNum) {
  return Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum
 }
 
+
+let arrData = [...data]
+
+setInterval(() => {
+	if(arrData[randomRange(0, arrData.length - 1)]) {
+		modal.textContent = `find ${arrData[randomRange(0, arrData.length - 1)]}`
+	} else {
+		modal.textContent = 'You win!'
+	}
+}, 15000)
+
+function getId (evt) {
+	modal.textContent = `find ${arrData[randomRange(0, arrData.length - 1)]}`
+
+	let foundImg = arrData.indexOf(evt.target.dataset.id)
+	arrData.splice(foundImg, 1)
+	evt.target.remove()
+}
