0a2913d57e7655e07b43dab8056da620e1461999
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-02-12 20:08:14 +0500

    Add timer

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 import { data } from './data.js'
 
+let arrData = shuffle(data)
+
 window.onload = (evt) => {
 
-	for(let d of data) {
+	for(let d of arrData) {
 
 		let x = randomRange(0, window.innerWidth)
 		let y = randomRange(0, window.innerHeight)
@@ -10,32 +12,58 @@ window.onload = (evt) => {
 		let img = document.createElement('IMG')
 		img.src = `./img/${d}.png`
 		img.dataset.id = d
-		img.style.cssText = `left: ${Math.abs(x - 128)}px; top:${Math.abs(y - 128)}px;`
+		img.style.cssText = `left: ${Math.abs(x - 156)}px; top:${Math.abs(y - 128)}px;`
 		img.onclick = getId
 
 		box.appendChild(img)
 	}
 }
 
-function randomRange(minNum, maxNum) {
- return Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum
-}
-
-
-let arrData = [...data]
-
-setInterval(() => {
-	if(arrData[randomRange(0, arrData.length - 1)]) {
-		modal.textContent = `find ${arrData[randomRange(0, arrData.length - 1)]}`
-	} else {
-		modal.textContent = 'You win!'
-	}
-}, 15000)
+let index = arrData.length - 1
+modal.textContent = `find ${arrData[index]}`
 
 function getId (evt) {
-	modal.textContent = `find ${arrData[randomRange(0, arrData.length - 1)]}`
+	if(evt.target.dataset.id === arrData[index]) {
+		let foundImg = arrData.indexOf(evt.target.dataset.id)
+		arrData.splice(foundImg, 1)
+		evt.target.remove()
 
-	let foundImg = arrData.indexOf(evt.target.dataset.id)
-	arrData.splice(foundImg, 1)
-	evt.target.remove()
+		index--
+		if(arrData[index]) {
+			modal.textContent = `find ${arrData[index]}`
+		} else {
+			modal.textContent = 'You win!'
+			modal.classList.add('green')
+		}		
+	} else {
+		modal.classList.add('shake')
+		setTimeout(() => {
+			modal.classList.remove('shake')
+		}, 1000)
+	}
 }
+
+function randomRange(minNum, maxNum) {
+	return Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum
+}
+
+function shuffle(arr) {
+	const copiedArr = [...arr]
+	return copiedArr.sort(() => Math.random() - 0.5)
+}
+
+let sec = arrData.length * 3
+
+let intervalId = setInterval(() => {
+	modalTime.textContent = sec
+
+	if(sec === 0) {
+		window.clearInterval(intervalId)
+
+		modal.textContent = 'You loose!'
+		modal.classList.add('alert')
+	}	else {
+		sec--
+	}
+
+}, 1000)
